cf8a4bb0ee5aaf785ecd29de3ced6edaf2b16cab
tobi <tobi19.py@gmail.com>
2024-12-16 14:00:57 -0600

    Implement character report

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def main():
     path = "books/frankenstein.txt"
     book = get_book(path)
-    count = get_book_words(book)
-    char_count = get_chars_count(book)
-    print(count)
-    print(char_count)
+    word_count = get_book_words(book)
+    char_dict = get_chars_count(book)
+    sorted_list = get_report(char_dict)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    print()
+    for char_info in sorted_list:
+        print(f"The '{char_info["char"]}' character was found {char_info["count"]} times")
+
+    print("--- End report ---")
 
 def get_book(path):
     with open(path) as f:
@@ -13,6 +20,15 @@ def get_book(path):
 def get_book_words(book):
     words = book.split()
     return len(words)
+
+def get_report(char_dict):
+    char_list = []
+
+    for key, val in char_dict.items():
+        if key.isalpha():
+            char_list.append({"char": key, "count": val})
+    char_list.sort(reverse=True, key=lambda x: x["count"])
+    return char_list
 
 def get_chars_count(book):
     char_dict = {}
