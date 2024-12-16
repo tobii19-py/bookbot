ed6d27d32b2c1341670f84b6ef972579ff2079ae
tobi <tobi19.py@gmail.com>
2024-12-16 12:22:08 -0600

    Implement character count

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     path = "books/frankenstein.txt"
     book = get_book(path)
     count = get_book_words(book)
+    char_count = get_chars_count(book)
     print(count)
+    print(char_count)
 
 def get_book(path):
     with open(path) as f:
@@ -11,5 +13,18 @@ def get_book(path):
 def get_book_words(book):
     words = book.split()
     return len(words)
+
+def get_chars_count(book):
+    char_dict = {}
+
+    for ch in book:
+        ch_lower = ch.lower()
+    
+        if ch_lower in char_dict:
+            char_dict[ch_lower] += 1
+        else:
+            char_dict[ch_lower] = 1
+    
+    return char_dict
 
 main()
